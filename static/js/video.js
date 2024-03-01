ee82529940ac2d3043bde06eec584a1db7903306
Zorglube <630192+zorglube@users.noreply.github.com>
2024-03-01 16:32:29 +0100

    Update video.js
    Try add header

--- static/js/video.js
+++ static/js/video.js
@@ -10,7 +10,8 @@ function initPlayer() {
     let videoElement = document.querySelector('#videoElement');
     let flvPlayer = flvjs.createPlayer({
         type: 'flv',
-        url: '/live'
+        url: '/live',
+        headers: {"content-lenght: 0"},
     });
     flvPlayer.attachMediaElement(videoElement);
     flvPlayer.load();
